35e8db20352406234ec27f8cd5e6678ff553c4c1
TalelMejri <talelmejri8@gmail.com>
2023-09-11 14:52:06 +0100

    fix Location

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -29,13 +31,47 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+
+Location _location=Location();
+late LocationData _locationData;
+
+Future<void> _getPosition() async{
+  try{
+    _locationData=await _location.getLocation();
+  }catch(e){  
+    print('$e');
+  }
+}
+
+ @override
+  void initState() {
+    super.initState();
+    _getPosition();
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        leading: const Icon(Icons.favorite),
       ),
+      body: Center(child: Padding(padding: const EdgeInsets.all(15),child: 
+      ElevatedButton(child: const Icon(Icons.local_activity),onPressed: (){
+        showModalBottomSheet(context: context, builder:(BuildContext context){
+          return Center(
+            child: Column(children: [
+              const Text("Location"),
+              Text("Logitutde ${_locationData.longitude}"),
+              Text("latitude ${_locationData.latitude}")
+            ]),
+          );
+        });
+      },),
+      ),
+      )
     );
   }
 }
